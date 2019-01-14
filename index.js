29070a5de5cb7061e7ebc3bcd9a3346eb1c5980f
kevinsbstn4 <kevinsbstn4@gmail.com>
2019-01-14 08:29:59 +0700

    menambah premitive text

---
+++ index.js
@@ -0,0 +1,6 @@
+let Nama = "Kevin";             // String
+let Umur = 15;                 // Number
+let isApproved = true;        // Boolean
+let firstName = undefined    // Undefined
+let Color = null            // Null
+
